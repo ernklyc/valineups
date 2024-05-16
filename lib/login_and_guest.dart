77ff86a913b7fc881a456e88369e4891f1eb854e
Eren KALAYCI <klychub@gmail.com>
2024-05-16 18:39:12 +0300

    update

---
+++ lib/login_and_guest.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LoginAndGuest extends StatelessWidget {
+  const LoginAndGuest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("VALINEUPS"),
+          ElevatedButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                Text("data"),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                Text("data"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:valineups/login_and_guest.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'VALINEUPS',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginAndGuest(),
+    );
+  }
+}
