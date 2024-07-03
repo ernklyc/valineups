56fb09e7c8f46bf9e88d14989506bb5dd53448e3
Eren KALAYCI <klychub@gmail.com>
2024-07-03 11:32:22 +0300

    assets update

--- lib/localization/strings.dart
+++ lib/localization/strings.dart
@@ -4,10 +4,9 @@ class AuthPageText {
   final String l = "L";
   final String ineups = "INEUPS";
 
-  final String googleAuth = "assets/images/googleAuth.png";
-  final String anonimAuth = "assets/images/anonimAuth.png";
-
-  final String loginGuestPageCover = "assets/images/valoAgent.gif";
+  final String googleAuth = "assets/images/loginScreen/googleAuth.png";
+  final String anonimAuth = "assets/images/loginScreen/anonimAuth.png";
+  final String loginGuestPageCover = "assets/images/loginScreen/valoAgent.gif";
   final String infoText = "Valorant Unofficial Fan App";
 
   final String google = "Google";
@@ -19,17 +18,17 @@ class OnBoardingScreen {
   final String next = "Next";
   final String start = "Start";
 
-  final String agents = "assets/images/ajan.png";
+  final String agents = "assets/images/onBoardingScreen/ajan.png";
   final String agentTitle = "Hoş Geldin, Ajan!";
   final String agentDesc =
       "Valaron Lineups ile oyun içindeki stratejini güçlendir! Beğendiğin lineupsları kaydet ve arkadaşlarınla paylaş. Hazır mısın? Hadi başlayalım!";
 
-  final String lineups = "assets/images/bomba.png";
+  final String lineups = "assets/images/onBoardingScreen/bomba.png";
   final String lineupTitle = "Lineups Hazırlığı";
   final String lineupDesc =
       "Oyun sırasında hızlı ve etkili lineupslar keşfet. En sevdiğin stratejileri kaydet ve her zaman elinin altında olsun. Rakiplerini şaşırtmaya hazır ol!";
 
-  final String discussions = "assets/images/discussions.png";
+  final String discussions = "assets/images/onBoardingScreen/discussions.png";
   final String discussionTitle = "Ortak Sohbet Alanı";
   final String discussionDesc =
       "Stratejilerini paylaş ve diğer oyuncularla sohbet et. Ortak sohbet alanımızda fikir alışverişi yap ve yeni arkadaşlar edin. Birlikte güçlüyüz!";
