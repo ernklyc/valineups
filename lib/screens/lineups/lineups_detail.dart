2966def07b37f218e1fd06a0303634298fa59cca
Eren KALAYCI <klychub@gmail.com>
2024-07-26 10:41:19 +0300

    update

--- lib/screens/lineups/lineups_detail.dart
+++ lib/screens/lineups/lineups_detail.dart
@@ -16,13 +16,21 @@ class LineupDetailScreen extends StatefulWidget {
 }
 
 class _LineupDetailScreenState extends State<LineupDetailScreen> {
+  late PageController _pageController;
   int _currentImageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentImageIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
     final imagePaths = List<String>.from(widget.lineup['imagePaths']);
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         actions: [
           IconButton(
             icon: Icon(Icons.circle, color: ProjectColor().dark),
@@ -42,122 +50,43 @@ class _LineupDetailScreenState extends State<LineupDetailScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              children: [
-                CarouselSlider(
-                  options: CarouselOptions(
-                    height: 400.0,
-                    viewportFraction: 1.0,
-                    enlargeCenterPage: false,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _currentImageIndex = index;
-                      });
-                    },
-                  ),
-                  items: imagePaths.map((imagePath) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => FullScreenImageGallery(
-                                  imagePaths: imagePaths,
-                                  initialIndex: _currentImageIndex,
-                                ),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12.0),
-                              image: DecorationImage(
-                                image: NetworkImage(imagePath),
-                                fit: BoxFit.fitWidth,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }).toList(),
+            Expanded(
+              child: PhotoViewGallery.builder(
+                pageController: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentImageIndex = index;
+                  });
+                },
+                itemCount: imagePaths.length,
+                builder: (context, index) {
+                  return PhotoViewGalleryPageOptions(
+                    imageProvider: NetworkImage(imagePaths[index]),
+                    initialScale: PhotoViewComputedScale.contained,
+                    minScale: PhotoViewComputedScale.contained,
+                    maxScale: PhotoViewComputedScale.covered * 2,
+                  );
+                },
+                scrollPhysics: BouncingScrollPhysics(),
+                backgroundDecoration: BoxDecoration(
+                  color: ProjectColor().dark,
                 ),
-                SizedBox(height: 10),
-                Center(
-                  child: AnimatedSmoothIndicator(
-                    activeIndex: _currentImageIndex,
-                    count: imagePaths.length,
-                    effect: ScrollingDotsEffect(
-                      activeDotColor: ProjectColor().white,
-                      dotColor: ProjectColor().white.withOpacity(0.5),
-                      dotHeight: 8.0,
-                      dotWidth: 8.0,
-                    ),
-                  ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: AnimatedSmoothIndicator(
+                activeIndex: _currentImageIndex,
+                count: imagePaths.length,
+                effect: ScrollingDotsEffect(
+                  activeDotColor: ProjectColor().white,
+                  dotColor: ProjectColor().white.withOpacity(0.5),
+                  dotHeight: 8.0,
+                  dotWidth: 8.0,
                 ),
-              ],
+              ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class FullScreenImageGallery extends StatefulWidget {
-  final List<String> imagePaths;
-  final int initialIndex;
-
-  FullScreenImageGallery(
-      {required this.imagePaths, required this.initialIndex});
-
-  @override
-  _FullScreenImageGalleryState createState() => _FullScreenImageGalleryState();
-}
-
-class _FullScreenImageGalleryState extends State<FullScreenImageGallery> {
-  late PageController _pageController;
-  late int _currentIndex;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentIndex = widget.initialIndex;
-    _pageController = PageController(initialPage: _currentIndex);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ProjectColor().dark,
-        title:
-            Text("Image ${_currentIndex + 1} of ${widget.imagePaths.length}"),
-      ),
-      backgroundColor: ProjectColor().dark,
-      body: PhotoViewGallery.builder(
-        pageController: _pageController,
-        onPageChanged: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        itemCount: widget.imagePaths.length,
-        builder: (context, index) {
-          return PhotoViewGalleryPageOptions(
-            imageProvider: NetworkImage(widget.imagePaths[index]),
-            initialScale: PhotoViewComputedScale.contained,
-            minScale: PhotoViewComputedScale.contained,
-            maxScale: PhotoViewComputedScale.covered * 2,
-          );
-        },
-        scrollPhysics: BouncingScrollPhysics(),
-        backgroundDecoration: BoxDecoration(
-          color: ProjectColor().dark,
         ),
       ),
     );
