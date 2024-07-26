29d2c99d717a32ed7326e5730aa16247481fd959
Eren KALAYCI <klychub@gmail.com>
2024-07-26 14:04:07 +0300

    saved kısmına fotoğğraf yakınlaştırma eklendi

--- lib/screens/home/profile.dart
+++ lib/screens/home/profile.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:random_avatar/random_avatar.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:valineups/components/full_screen_image_viewer.dart';
 import 'package:valineups/components/sides.dart';
 import 'package:valineups/styles/fonts.dart';
 import 'package:valineups/styles/project_color.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -293,8 +295,8 @@ class _ProfileState extends State<Profile> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => FullScreenImageViewer(
-                          images: List<String>.from(map['images']),
+                        builder: (context) => LineupDetailScreen(
+                          lineup: map,
                         ),
                       ),
                     );
@@ -388,6 +390,93 @@ class _ProfileState extends State<Profile> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class LineupDetailScreen extends StatefulWidget {
+  final Map<String, dynamic> lineup;
+
+  LineupDetailScreen({required this.lineup});
+
+  @override
+  _LineupDetailScreenState createState() => _LineupDetailScreenState();
+}
+
+class _LineupDetailScreenState extends State<LineupDetailScreen> {
+  late PageController _pageController;
+  int _currentImageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentImageIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final imagePaths = List<String>.from(widget.lineup['images']);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.circle, color: ProjectColor().dark),
+            onPressed: () {},
+          ),
+        ],
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: ProjectColor().white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: ProjectColor().dark,
+      ),
+      backgroundColor: ProjectColor().dark,
+      body: Padding(
+        padding: const EdgeInsets.all(1.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: PhotoViewGallery.builder(
+                pageController: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentImageIndex = index;
+                  });
+                },
+                itemCount: imagePaths.length,
+                builder: (context, index) {
+                  return PhotoViewGalleryPageOptions(
+                    imageProvider: NetworkImage(imagePaths[index]),
+                    initialScale: PhotoViewComputedScale.contained,
+                    minScale: PhotoViewComputedScale.contained,
+                    maxScale: PhotoViewComputedScale.covered * 2,
+                  );
+                },
+                scrollPhysics: BouncingScrollPhysics(),
+                backgroundDecoration: BoxDecoration(
+                  color: ProjectColor().dark,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: AnimatedSmoothIndicator(
+                activeIndex: _currentImageIndex,
+                count: imagePaths.length,
+                effect: ScrollingDotsEffect(
+                  activeDotColor: ProjectColor().white,
+                  dotColor: ProjectColor().white.withOpacity(0.5),
+                  dotHeight: 8.0,
+                  dotWidth: 8.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
